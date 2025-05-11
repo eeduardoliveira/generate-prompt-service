7c6e5b781cdc77d8ce9b7d7f10decee6edc20ca4
Eduardo R. Oliveira <eduardo.ribeiro.oliveira23@gmail.com>
2025-05-11 05:59:16 +0100

    add cors

--- main.go
+++ main.go
@@ -10,9 +10,11 @@ import (
 	"generate-prompt-service/dependencies/uploader"
 	handler "generate-prompt-service/presentation/http"
 
-	_ "generate-prompt-service/docs"               // Swagger docs (gerado pelo swag init)
-	httpSwagger "github.com/swaggo/http-swagger"  // Swagger handler
+	_ "generate-prompt-service/docs" // Swagger docs (gerado pelo swag init)
+
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+	httpSwagger "github.com/swaggo/http-swagger" // Swagger handler
 )
 
 // @title Generate Prompt Service API
@@ -54,8 +56,17 @@ func main() {
 	// Rota do Swagger
 	http.HandleFunc("/swagger/", httpSwagger.WrapHandler)
 
+
+		// Middleware de CORS
+		corsHandler := cors.New(cors.Options{
+			AllowedOrigins:   []string{"*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Authorization", "Content-Type"},
+			AllowCredentials: true,
+		}).Handler(http.DefaultServeMux)
+
 	log.Printf("🚀 Servidor rodando na porta %s...", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	if err := http.ListenAndServe(":"+port, corsHandler); err != nil {
 		log.Fatalf("Erro ao iniciar servidor: %v", err)
 	}
 }
